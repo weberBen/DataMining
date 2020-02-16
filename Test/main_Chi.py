08f00e5f5004926202fafad9c21d1b0e7f027673
chatdok <49732490+chatdok@users.noreply.github.com>
2020-02-16 15:25:32 +0100

    Update main_Chi.py

--- Test/main_Chi.py
+++ Test/main_Chi.py
@@ -5,6 +5,8 @@
 #%%
 import environment as Env
 import sys
+from querySystem.matrixOp import *
+from dataManagement.Dictionnary import WordsBag
 
 info = Env.Info()
 '''
@@ -16,5 +18,12 @@ env_obj = Env.setupEnv(sys.argv[0], info)
 database = env_obj.Database
 wordsBag = env_obj.WordsBag
 
+if __name__ == "__main__":
+    A, V = createTFMatrixV2("./querySystem", mute = False)
+    print(A.toarray())
+    print(V.toarray())
+    print(wordsBag.getIds("Bonjour"))
+    Q = createQueryVect(wordsBag, input("Recherche : "))
+    print(Q)
 
 #%%
