518ebf06d93913123387ab82bd24563fab263bd5
chatdok <49732490+chatdok@users.noreply.github.com>
2020-02-19 19:45:48 +0100

    Update main_Youssef.py

--- Test/main_Youssef.py
+++ Test/main_Youssef.py
@@ -7,33 +7,45 @@ import environment as Env
 import sys, os
 import Frequency.SummaryWordFrequency as Freq
 
-#info = Env.Info()
+info = Env.Info()
 '''
 wordsBagInfo = Env.WordsBagInfo(ignore=True)
 info = Env.Info(wordsBagInfo=wordsBagInfo)
 '''
+'''
+env_obj = Env.setupEnv([__file__, sys.argv[0], os.getcwd()], info)
+database = env_obj.Database
+wordsBag = env_obj.WordsBag'''
+F=Freq.Frequency(None,None, "MoviesFrequence.txt")
 
-#env_obj = Env.setupEnv([__file__, sys.argv[0], os.getcwd()], info)
-#database = env_obj.Database
-#wordsBag = env_obj.WordsBag
-
-#F=Freq.Frequency(database,wordsBag)
+it = F.iterator2()
+cpt = 0
+while it.hasNext():
+    print("&&&&&&&&&&&&&&&&& MOVIE  &&&&&&&&&&&&&&&&&")
+    m = it.getNext()
+    print("\tid_movie="+str(m.id))
+    itt = m.iterator()
+    
+    print("----> itration freq movie")
+    while itt.hasNext():
+        print("id_word, freq=", itt.getNext())
+    
+    cpt+=1
+    
+    if cpt==3:
+        break
 #print(F._filename)
 #F.computeFrequency()
-a=Freq.MovieFreq("MoviesFrequence.txt","0")
-print(a.file)
-it=a.iterator()
-#print(a.file.read())
-cpt=0
-#print(it.hasNext)
 
-while cpt<2:
-    #print(cpt)
+'''
+F2=Freq.MovieFreq("MoviesFrequence.txt","0")
+it=F2.iterator()
+
+cpt=0
+while cpt<4:
     test = it.getNext()
     print(test)
     cpt+=1
-    
-
-
+'''
 
 #%%
