5d5d2bac053bc5046d277b1e8c7b97ab0cfef917
chatdok <49732490+chatdok@users.noreply.github.com>
2020-02-19 19:44:12 +0100

    Update matrixOp.py

--- Test/querySystem/matrixOp.py
+++ Test/querySystem/matrixOp.py
@@ -18,7 +18,7 @@ from time import perf_counter
 
 sys.path.insert(0, '..')
 
-freqFormat = "/*_freq.txt"
+import Frequency.SummaryWordFrequency as SWF
 
 ###############################################
 #--------------------CLASS--------------------#
@@ -26,7 +26,7 @@ freqFormat = "/*_freq.txt"
 
 class TDMvm:
     def __init__(self, path):
-        self.mat, self.idf = createTFMatrixV2(path)
+        self.mat, self.idf = createTFMatrixV3(path)
 
     def toStr(self):
         print("Matrice Termes-Documents (TF) :\n",self.mat.toarray())
@@ -39,9 +39,9 @@ class TDMvm:
 def addDocumentToMatrix(M, V, mat):
     pass
 
-def createTFMatrixV2(path = ".", mute = True):
+def createTFMatrixV3(N, mute = True):
     """
-    string * bool -> CSC | None
+    int * bool -> CSC | None
     Retourne la matrice termes-documents TF dont
     les coefficients sont les fréquences de chaque mot
     dans les documents
@@ -54,52 +54,35 @@ def createTFMatrixV2(path = ".", mute = True):
         print("createTFMatrixV2 - Taille CSC : "+str(sys.getsizeof(M))+" bytes")
         print("createTFMatrixV2 - Taille Vecteur IDF : "+str(sys.getsizeof(V))+" bytes")
 
-    lst_dwc = glob.glob(path+freqFormat)
+    #lst_dwc = glob.glob(path+freqFormat)
     data = []
     indices = []
     indptr = [0]
-    #totalWordCount = 0
-    table = {}
-    isLineEmpty = False
+    table = array.array('i', [])
     headIndex = 0
     tailIndex = 0
+    i = 0
+    subTotal = 0
     start = perf_counter()
-    for filename in lst_dwc:
-        isLineEmpty = False
+    F = SWF.Frequency(None, None, "../MoviesFrequence.txt")
+    it = F.iterator2()
+    while i < N and it.hasNext():
+        m = it.getNext()
+        table.append(m.id)
+        itt = m.iterator()
         subTotal = 0
-        try:
-            file = open(filename, "r")
-            for line in file.readlines():
-                content = line.strip('\n').split(' ')
-                if len(content) == 0:
-                    isLineEmpty = True
-                    continue
-                wid, wct = int(content[0]), int(content[-1])
-                table.setdefault(wid, len(table))
-
-                data.append(wct)
-                indices.append(table[wid])
-
-                subTotal += wct
-                tailIndex += 1
-            file.close()
-            if isLineEmpty:
-                continue
-            #totalWordCount += subTotal
-            indptr.append(tailIndex-headIndex+indptr[-1])
-            """
-            for i in range(headIndex,tailIndex):
-                data[i] = float(data[i])/subTotal
-            headIndex = tailIndex
-            """
-            #i = headIndex
-            while headIndex < tailIndex:
-                data[headIndex] = float(data[headIndex])/subTotal
-                headIndex += 1
+        while itt.hasNext():
+            wid,wct = itt.getNext()
+            data.append(wct)
+            indices.append(wid)
+            subTotal += wct
+            tailIndex += 1
+        indptr.append(tailIndex-headIndex+indptr[-1])
+        while headIndex < tailIndex:
+            data[headIndex] = float(data[headIndex])/subTotal
             headIndex += 1
-        except IOError:
-            print("Erreur de lecture (IOError)")
-            return None
+        headIndex += 1
+        i += 1
     M = scs.csc_matrix((data, indices, indptr), dtype = float)
     m,n = M.shape
     i = 0
@@ -112,7 +95,8 @@ def createTFMatrixV2(path = ".", mute = True):
     if not mute:
         varsizecheck(data, indices, indptr, M, V)
         print("createTFMatrixV2 - Temps pris : "+str(end-start)+"s")
-    return M, V
+    return M, V, table
+
 
 def createQueryVect(wordsbag, sentence, mute = True):
     """
@@ -188,10 +172,15 @@ def main(args = None):
     # la matrice CSR en matrice normale
     #print(createTFMatrixV1().toarray())
     #print("")
+    print(os.getcwd())
     print("Création de la matrice termes-documents")
-    A, V = createTFMatrixV2(mute = False)
+    N = int(input("Nombre de films à inclure : "))
+    A, V, table = createTFMatrixV3(N, mute = False)
     print(A.toarray())
+    print(A.shape)
     print(V.toarray())
+    print(V.shape)
+    #print(table)
     #print(A.nonzero())
     s = input("Recherche : ")
     Q = createQueryVect(s, mute = False)
@@ -251,6 +240,83 @@ def DEPRECATEDcreateTFMatrixV1(path = "."):
     M = scs.csr_matrix((data, indices, indptr), dtype = float)
     print("createTFMatrixV1 - Taille CSR : "+str(sys.getsizeof(M))+" bytes")
     return M
+
+def DEPRECATEDcreateTFMatrixV2(path = ".", mute = True):
+    """
+    string * bool -> CSC | None
+    Retourne la matrice termes-documents TF dont
+    les coefficients sont les fréquences de chaque mot
+    dans les documents
+    """
+    freqFormat = "/*_freq.txt"
+    
+    def varsizecheck(data, indices, indptr, M, V):
+        print("createMatrixV2 - Tailles des var intermédiaires")
+        print("sizeof(data) : "+str(sys.getsizeof(data)))
+        print("sizeof(indices) : "+str(sys.getsizeof(indices)))
+        print("sizeof(indptr) : "+str(sys.getsizeof(indptr)))
+        print("createTFMatrixV2 - Taille CSC : "+str(sys.getsizeof(M))+" bytes")
+        print("createTFMatrixV2 - Taille Vecteur IDF : "+str(sys.getsizeof(V))+" bytes")
+
+    lst_dwc = glob.glob(path+freqFormat)
+    data = []
+    indices = []
+    indptr = [0]
+    #totalWordCount = 0
+    table = {}
+    isLineEmpty = False
+    headIndex = 0
+    tailIndex = 0
+    start = perf_counter()
+    for filename in lst_dwc:
+        isLineEmpty = False
+        subTotal = 0
+        try:
+            file = open(filename, "r")
+            for line in file.readlines():
+                content = line.strip('\n').split(' ')
+                if len(content) == 0:
+                    isLineEmpty = True
+                    continue
+                wid, wct = int(content[0]), int(content[-1])
+                table.setdefault(wid, len(table))
+
+                data.append(wct)
+                indices.append(table[wid])
+
+                subTotal += wct
+                tailIndex += 1
+            file.close()
+            if isLineEmpty:
+                continue
+            #totalWordCount += subTotal
+            indptr.append(tailIndex-headIndex+indptr[-1])
+            """
+            for i in range(headIndex,tailIndex):
+                data[i] = float(data[i])/subTotal
+            headIndex = tailIndex
+            """
+            #i = headIndex
+            while headIndex < tailIndex:
+                data[headIndex] = float(data[headIndex])/subTotal
+                headIndex += 1
+            headIndex += 1
+        except IOError:
+            print("Erreur de lecture (IOError)")
+            return None
+    M = scs.csc_matrix((data, indices, indptr), dtype = float)
+    m,n = M.shape
+    i = 0
+    V = array.array('f')
+    while i < m:
+        V.append(np.log(n/max(0.001, M.getrow(i).count_nonzero())))
+        i += 1
+    V = scs.csc_matrix((V, range(len(V)), [0, len(V)]), dtype = float)
+    end = perf_counter()
+    if not mute:
+        varsizecheck(data, indices, indptr, M, V)
+        print("createTFMatrixV2 - Temps pris : "+str(end-start)+"s")
+    return M, V
 
 def DEPRECATEDconvertToTFIDF(M):
     """
