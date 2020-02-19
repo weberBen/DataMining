523a3d61765d7b0b0e61b59792666ac5aeb1ea5c
chatdok <49732490+chatdok@users.noreply.github.com>
2020-02-19 20:40:17 +0100

    Update main_Chi.py

--- Test/main_Chi.py
+++ Test/main_Chi.py
@@ -4,7 +4,7 @@
 
 #%%
 import environment as Env
-import sys
+import sys, os
 from querySystem.matrixOp import *
 
 info = Env.Info()
@@ -18,13 +18,16 @@ database = env_obj.Database
 wordsBag = env_obj.WordsBag
 
 if __name__ == "__main__":
-    N = 10
-    A, V, table = createTFMatrixV3(N, mute = False)
+    N = int(input("Nombre de documents : "))
+    print("Création de la matrice termes-documents sur les "+str(N)+" premiers films")
+    A, V, table = createTFMatrixV3(N, "MoviesFrequence.txt", mute = False)
     print(A.toarray())
     print(V.toarray())
     print(table)
     print(wordsBag.getIds("Bonjour"))
     Q = createQueryVect(wordsBag, input("Recherche : "))
     print(Q)
+    imax, maxsco = getMostRelevantDoc(A, V, Q, mute = False)
+    print("Score max : "+str(maxsco)+"\nDocument : "+str(table[imax]))
 
 #%%
