2a24d4214009c5c71cbb1c9a9c79cae73959c35a
Youssef <48862755+Yurof@users.noreply.github.com>
2020-04-19 18:02:17 +0200

    flask

--- Test/app.py
+++ Test/app.py
@@ -19,30 +19,39 @@ def home():
 @app.route('/', methods=['POST'])
 def text_box():
     text = request.form['text']
+    ma = request.form["matrice"]
     processed_text = text.upper()
-    tmp=recherche(processed_text)
+    tmp=recherche(processed_text,ma)
     print(tmp)
+    
 
-    return render_template("resultat.html" , message = tmp )
+    return render_template("resultat.html" , message1 = tmp )
 
 
-def recherche(input):
+def recherche(input,ma):
     info = Env.Info()
     env_obj = Env.setupEnv([__file__, sys.argv[0], os.getcwd()], info)
     database = env_obj.Database
     wordsBag = env_obj.WordsBag
     Freq = env_obj.Frequency
-    matrix = "matrix_"+"all"
+    matrix = ma
     r = Request(database, wordsBag, Freq, env_obj.getMatrixFolder())
     r.load(matrix)
 
     response = r.search(input, 1)
     res = response.results
+    if len(res)==0:
+            return("None","0")
+            
     movie_id, score  = res[0]
     movie = database.getMovie(movie_id)
     print("\ntitre du film\n"+movie.title)
-    return movie.title
 
+    return (movie.title,round(score,2))
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run()
